e42769d3bbacd6dbbdbea98fc54da25cca839d96
Ailson Araujo <ailson.a.leite@gmail.com>
2020-09-13 09:09:22 -0300

    classe de inicialização do gráfico na interface

---
+++ c_mplwidget.py
@@ -0,0 +1,30 @@
+########################################################################
+## MIT License
+## Copyright © 2020, Ailson Araujo.
+##
+## Autor: Ailson Araujo
+## Versão: 1.0.0
+## Iniciado: 29/08/2020
+## Finalizado:
+##
+## Este projeto pode ser usado livremente, desde que mantenham
+## os respectivos créditos apenas nos scripts Python.
+## Projeto feito com Qt Designer e PyQt5
+########################################################################
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QWidget, QHBoxLayout
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+
+class MplWidget(QWidget):
+
+    def __init__(self, parent = None):
+        QWidget.__init__(self, parent)
+
+        self.canvas = FigureCanvasQTAgg(Figure(figsize = (10, 10), dpi= 100, facecolor='#282c34'))
+        vertical_layout = QHBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+
+        self.axes = self.canvas.figure.subplots()
+        self.setLayout(vertical_layout)
