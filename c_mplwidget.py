93b07ecf79991d146c0b44622a15e7701f19fc6b
Ailson Araujo <ailson.a.leite@gmail.com>
2020-10-03 10:45:19 -0300

    licença atualizada

--- c_mplwidget.py
+++ c_mplwidget.py
@@ -1,15 +1,25 @@
 ########################################################################
-## MIT License
-## Copyright © 2020, Ailson Araujo.
+## Copyright © 2020 Ailson Araujo
 ##
-## Autor: Ailson Araujo
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
 ## Versão: 1.0.0
 ## Iniciado: 29/08/2020
 ## Finalizado:
 ##
 ## Este projeto pode ser usado livremente, desde que mantenham
-## os respectivos créditos apenas nos scripts Python.
-## Projeto feito com Qt Designer e PyQt5
+## os respectivos créditos nos scripts Python.
+## Projeto feito com Qt Designer, PyQt5 e Matplotlib
 ########################################################################
 
 from PyQt5 import QtWidgets
